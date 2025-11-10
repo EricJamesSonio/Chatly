b1af9908bab9e9863ea11d4ea75292701b4b954f
EricJamesSonio <ericjamessonio7@gmail.com>
2025-11-11 01:28:45 +0800

    asd

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -4,23 +4,25 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
-// Resolve the path to the root .env file
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
-export let db;
+export const db = await mysql.createPool({
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || 'chatly',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
 try {
-  db = await mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3307,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'chatly'
-  });
-
+  const conn = await db.getConnection();
   console.log('✅ Connected to MySQL Database');
+  conn.release();
 } catch (err) {
   console.error('❌ Database connection failed:', err.message);
   process.exit(1);
