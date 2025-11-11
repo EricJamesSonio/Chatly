942ccab05717d4726a1e7e4a0c123559a24d74aa
EricJamesSonio <ericjamessonio7@gmail.com>
2025-11-11 22:28:11 +0800

    Fix wrong api call

--- client/src/components/friends/FriendButtons.tsx
+++ client/src/components/friends/FriendButtons.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../css/FriendButton.css";
-import { useFriends } from "../../context/FriendsContext"; // ✅ import hook
-const API_URL = import.meta.env.VITE_API_URL;
+import { useFriends } from "../../context/FriendsContext";
+const API_URL = import.meta.env.VITE_API_URL || '';
 
 interface FriendButtonsProps {
   currentUserId: number;
